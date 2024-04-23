bc3f1d5b0f74fa5aca3b77cee02900984f8c7273
ayendiki <ayendiki@mgh.harvard.edu>
2024-04-23 15:56:44 -0400

    Update and rename to Fibers.jl

---
+++ src/Fibers.jl
@@ -0,0 +1,21 @@
+module Fibers
+
+include("mri.jl")
+include("util.jl")
+include("trk.jl")
+include("show.jl")
+#include("view.jl")
+include("dti.jl")
+include("odf.jl")
+include("dsi.jl")
+include("gqi.jl")
+include("rusd.jl")
+include("stream.jl")
+
+function __init__()
+
+  println("FREESURFER_HOME: " * (haskey(ENV, "FREESURFER_HOME") ?
+                                 ENV["FREESURFER_HOME"] : "not defined"))
+end
+
+end # module
