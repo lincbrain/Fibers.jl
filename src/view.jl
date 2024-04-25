d0269ce3f710a84b2577e15de05b48c605a792f7
ayendiki <ayendiki@mgh.harvard.edu>
2024-04-24 23:03:04 -0400

    Removed old header

--- src/view.jl
+++ src/view.jl
@@ -1,15 +1,5 @@
 #=
-  Original Author: Anastasia Yendiki
-
-  Copyright © 2022 The General Hospital Corporation (Boston, MA) "MGH"
-
-  Terms and conditions for use, reproduction, distribution and contribution
-  are found in the 'FreeSurfer Software License Agreement' contained
-  in the file 'LICENSE' found in the FreeSurfer distribution, and here:
-
-  https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
-
-  Reporting: freesurfer@nmr.mgh.harvard.edu
+  Image display
 =#
 
 using ColorTypes, ImageView
