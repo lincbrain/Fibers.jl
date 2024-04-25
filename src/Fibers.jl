06569a586754855322f011482cbf9bb3a5021b3b
ayendiki <ayendiki@mgh.harvard.edu>
2024-04-24 23:03:42 -0400

    Disabled FS environment variable check

--- src/Fibers.jl
+++ src/Fibers.jl
@@ -14,8 +14,8 @@ include("stream.jl")
 
 function __init__()
 
-  println("FREESURFER_HOME: " * (haskey(ENV, "FREESURFER_HOME") ?
-                                 ENV["FREESURFER_HOME"] : "not defined"))
+#  println("FREESURFER_HOME: " * (haskey(ENV, "FREESURFER_HOME") ?
+#                                 ENV["FREESURFER_HOME"] : "not defined"))
 end
 
 end # module
